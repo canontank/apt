4bf5cf5f7c376ef7891578928007c525fbe8b0ad
canontank <58597814+canontank@users.noreply.github.com>
2021-04-01 14:15:57 +0900

    Add files via upload

---
+++ skview/js/skview.js
@@ -0,0 +1,14 @@
+var imgList = new Array(
+	'2020-10', '2020-11', '2020-12',
+	'2021-01', '2021-02'
+);
+
+$(document).ready(function() {
+    for (var img of imgList) {
+        $('#skview').append($('<div/>', { html : img.replace('-', '. ') }));
+        $('#skview').append($('<div/>', {})
+            .append($('<img/>', { src : '../skview/img/' + img + '.jpg' }))
+        );
+        $('#skview').append($('<div/>', { class : 'clear20' }));
+    }
+});
