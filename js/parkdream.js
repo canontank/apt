2c11aabfec1c4e73e6c1bb8c938975b84521f1bf
canontank <58597814+canontank@users.noreply.github.com>
2022-03-13 22:03:52 +0900

    Update parkdream.js

--- js/parkdream.js
+++ js/parkdream.js
@@ -1,45 +1,16 @@
 var aptName = 'parkdream';
-var startYear = 2018;
-var endYear = 2020;
+var imgList = new Array(
+                                     '2018-04', '2018-05', '2018-06', '2018-07', '2018-08', '2018-09', '2018-10', '2018-11', '2018-12',
+    '2019-01', '2019-02', '2019-03', '2019-04', '2019-05', '2019-06', '2019-07', '2019-08', '2019-09', '2019-10',            '2019-12',
+    '2020-01', '2020-02', '2020-03', '2020-04', '2020-05', '2020-06', '2020-07', '2020-08', '2020-09'
+);
 
 $(document).ready(function() {
-	setDiv();
-	setImage();
+    for (var img of imgList) {
+        $('#parkdream').append($('<div/>', { html : img.replace('-', '. ') }));
+        $('#parkdream').append($('<div/>', {})
+            .append($('<img/>', { src : 'img/' + aptName + '/' + img + '.jpg' }))
+        );
+        $('#parkdream').append($('<div/>', { class : 'clear20' }));
+    }
 });
-
-function setDiv() {
-	for (var year = startYear; year <= endYear; year++) {
-		for (var month = 1; month <= 12; month++) {
-			$('#' + aptName).append($('<div/>', {
-				id : year + '-' + getMonthStr(month),
-				html : year + '. ' + getMonthStr(month)
-			}));
-		}
-	}
-}
-
-function setImage() {
-	for (var year = startYear; year <= endYear; year++) {
-		for (var month = 1; month <= 12; month++) {
-			var img = new Image();
-			img.onload = function(a) {
-				var imgPath = a.path[0].currentSrc;
-				var divId = imgPath.substring(imgPath.lastIndexOf('/') + 1, imgPath.length).replace('.jpg', '');
-				$('#' + divId).append($('<img/>', { src : imgPath }));
-				$('#' + divId).append($('<div/>', { class : 'clear20' }));
-			}
-			img.onerror = function(a) {
-				var imgPath = a.path[0].currentSrc;
-				var divId = imgPath.substring(imgPath.lastIndexOf('/') + 1, imgPath.length).replace('.jpg', '');
-				$('#' + divId).empty();
-			}
-			img.src='img/' + aptName + '/' + year + '-' + getMonthStr(month) + '.jpg';
-		}
-	}
-}
-
-function getMonthStr(month) {
-	if (month < 10)
-		return "0" + month;
-	return month;
-}
