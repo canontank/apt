5e659eba2ace3f18f03348a35b9e2bf0ae90d2bb
canontank <58597814+canontank@users.noreply.github.com>
2022-03-14 12:41:58 +0900

    Update skview.js

--- js/skview.js
+++ js/skview.js
@@ -1,16 +1,16 @@
 var aptName = 'skview';
 var imgList = new Array(
-														  '2020-11', '2020-12',
+												       '2020-10', '2020-11', '2020-12',
     '2021-01', '2021-02', '2021-03', '2021-04', '2021-05', '2021-06', '2021-07', '2021-08', '2021-09', '2021-10', '2021-11', '2021-12',
     '2022-01', '2022-02'
 );
 
 $(document).ready(function() {
     for (var img of imgList) {
-        $('#parkdream').append($('<div/>', { html : img.replace('-', '. ') }));
-        $('#parkdream').append($('<div/>', {})
+        $('#' + aptName).append($('<div/>', { html : img.replace('-', '. ') }));
+        $('#' + aptName).append($('<div/>', {})
             .append($('<img/>', { src : 'img/' + aptName + '/' + img + '.jpg' }))
         );
-        $('#parkdream').append($('<div/>', { class : 'clear20' }));
+        $('#' + aptName).append($('<div/>', { class : 'clear20' }));
     }
 });
